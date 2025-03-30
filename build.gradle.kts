fd30b5ec30a56cd101f1a3d47d4ee8e58acfd69c
fyam <fung19971115@gmail.com>
2025-03-30 22:16:06 +0800

    Fix publish token

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,7 @@ intellijPlatform {
     }
 
     publishing {
-        token = file("certificate/passphrase.txt").readText().trim()
+        token = file("certificate/publish_token.txt").readText().trim()
         // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
